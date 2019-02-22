499455a38207f8fb66f8a272fddceec4433f9efc
noahwang <234082230@qq.com>
2019-02-22 23:01:31 +0800

    update

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/micro/go-config"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+	"crypto/md5"
 )
 
 func walk_dir(dirPth, suffix string) (files []string, err error) {
@@ -34,6 +37,15 @@ type BucketConfig struct {
 	ACCESS_KEY_ID string `json:"ACCESS_KEY_ID"`
 	ACCESS_KEY_SECRET string `json:"ACCESS_KEY_SECRET"`
 	BUCKET_NAME string `json:"BUCKET_NAME"`
+}
+
+func md5SumFile(file string) (value [md5.Size]byte, err error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
+	value = md5.Sum(data)
+	return value, nil
 }
 
 func main() {
@@ -71,13 +83,41 @@ func main() {
 		fmt.Println(err.Error())
 	}
 
-	var fileName = ""
+	var objectKey = ""
 	for _, file := range files {
 		if *name != "" {
-			fileName = *name + "/" + file
+			objectKey = *name + "/" + file
 		}
-		fmt.Println(file + " >>> " + fileName)
-		err = bucket.PutObjectFromFile(fileName, file)
+
+		fileMd5, err := md5SumFile(file)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		clientFileMd5String := hex.EncodeToString(fileMd5[:])
+
+		fmt.Println(clientFileMd5String + "     " + file + " >>> " + objectKey)
+
+		b, err := bucket.IsObjectExist(objectKey)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		if b {
+			h, err := bucket.GetObjectDetailedMeta(objectKey)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			serverFileMd5String := h.Get("Content-MD5")
+
+			if clientFileMd5String == serverFileMd5String {
+				continue
+			}
+		}
+
+		options := []oss.Option{
+			oss.Meta("Content-MD5", clientFileMd5String),
+		}
+		err = bucket.PutObjectFromFile(objectKey, file, options...)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
